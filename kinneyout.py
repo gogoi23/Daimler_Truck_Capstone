6a4197174580965a71dc003f1276c5fbcc8aa198
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2023-02-15 11:47:43 -0800

    removed old file selector

--- kinneyout.py
+++ kinneyout.py
@@ -261,22 +261,7 @@ st.set_page_config(
      layout="wide",
      initial_sidebar_state="expanded",
 )
-import tkinter as tk
-from tkinter import filedialog
 
-# Set up tkinter
-root = tk.Tk()
-root.withdraw()
-
-# Make folder picker dialog appear on top of other windows
-root.wm_attributes('-topmost', 1)
-
-# Folder picker button
-st.title('Folder Picker')
-st.write('Please select a folder:')
-clicked = st.button('Folder Picker')
-if clicked:
-    dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
 
 
 st.title("Welcome to the Kinney:Out Results Viewer")
